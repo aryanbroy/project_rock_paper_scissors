809ce24ed55dc47ee3701a301ef7ad00226af668
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2023-09-02 16:51:21 +0530

    do the first rock paper scissors exercise

--- main.js
+++ main.js
@@ -3,5 +3,32 @@ const index = Math.floor(Math.random() * choices.length);
 
 function getComputerChoice(){
     const compChoice = choices[index];
-    console.log(compChoice);
+    return compChoice;
 }
+
+function playRound(playerSelection, computerSelection){
+    
+    console.log(computerSelection)
+    
+    playerSelection = playerSelection.toLowerCase();
+    console.log(`Player: ${playerSelection}`)
+
+    let winMessage = `You won, ${playerSelection} beats ${computerSelection}`;
+
+    if(playerSelection === 'rock' && computerSelection === 'scissors'){
+        return ("winMessage");
+    }
+    else if(playerSelection === 'paper' && computerSelection === 'rock'){
+        return (winMessage);
+    }
+    else if(playerSelection === 'scissors' && computerSelection === 'paper'){
+        return (winMessage);
+    }
+    else if(playerSelection === computerSelection){
+        return ("Draw");
+    }
+    else{
+        return (`You lost, ${computerSelection} beats ${playerSelection}`);
+    }
+}
+console.log(playRound(playerSelection = prompt('rock, paper or scissors?'), computerSelection = getComputerChoice()))
